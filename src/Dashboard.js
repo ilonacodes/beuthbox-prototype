ec8ca80da4fc7910a69b1e25bf9e7cba08dddcb9
Ilona Demidenko <demiluri@gmail.com>
2018-10-02 23:25:43 +0200

    Add footer and fixed navigation on top

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,10 +3,12 @@ import {Navigation} from './Navigation';
 import styled from 'react-emotion';
 import recentVideo from './img/recent-video-1.jpg';
 import arrowRight from './img/arrow-right.svg.png';
+import facebook from './img/facebook.png';
+import instagram from './img/instagram.png';
 
 const ContentContainer = styled('div')`
   background-color: #222;
-  margin: 0 5px;
+  margin: 60px 5px 0 5px;
   padding: 25px 25px 40px 25px;
 `;
 
@@ -72,6 +74,20 @@ const Videos = styled('div')`
   img {
     margin: 0 10px;
   }
+`;
+
+const Footer = styled('div')`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  background-color: #00A5A5;
+  height: 60px;
+  
+  img {
+    margin: 16px 30px 0 0;
+    width: 30px;
+    height: 30px;
+  }
 `
 
 const Content = () => {
@@ -129,6 +145,10 @@ export const Dashboard = () => {
         <div>
             <Navigation />
             <Content />
+            <Footer>
+                <img src={facebook} />
+                <img src={instagram} />
+            </Footer>
         </div>
     );
 };
--- src/Navigation.js
+++ src/Navigation.js
@@ -39,6 +39,9 @@ const NavContainer = styled('div')`
   width: 100%;
   height: 60px;
   background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
   -webkit-box-shadow: 3px 3px 4px -1px rgba(0,0,0,0.75);
   -moz-box-shadow: 3px 3px 4px -1px rgba(0,0,0,0.75);
   box-shadow: 3px 4px 4px -1px rgba(0,0,0,0.75);
