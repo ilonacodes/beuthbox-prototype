aa334c643295d462fb944e61a720b3bb1882d9d8
Ilona Demidenko <demiluri@gmail.com>
2018-10-09 20:54:28 +0200

    Style the right side of video details

--- src/VideoDetails.js
+++ src/VideoDetails.js
@@ -3,7 +3,7 @@ import styled from 'react-emotion';
 import {Navigation} from './Navigation';
 import recentVideo1 from './img/recent-video-1.jpg';
 import {css} from 'emotion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Footer} from './Dashboard';
 import facebook from './img/facebook.png';
 import instagram from './img/instagram.png';
@@ -72,7 +72,7 @@ const nextVideos = css`
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
-  height: 300px;
+  height: 500px;
   
   @media (min-width: 769px) and (max-width: 1025px) {
      height: 500px;
@@ -101,19 +101,97 @@ const nextVideo = css`
     margin-left: 25%;
     color: #00A5A5;
   }
-  
 `;
 
+const Tags = styled('div')`
+  display: flex;
+  margin-top: 20px;
+  flex-wrap: wrap;
+  
+  p {
+    margin-right: 10px;
+    padding-top: 0;
+  }
+`;
+
+const Notes = styled('div')`
+   display: flex;
+   justify-content: space-between;
+   color: white;
+   margin-right: 30px;
+   
+   button {
+    color: white;
+    background-color: #00A5A5;
+    border: 0;
+    height: 22px;
+    border-radius: 5px;
+    align-self: center;
+    outline: 0;
+
+    
+    &:hover {
+      cursor: pointer;
+    }
+   }
+`;
+
+const hide = css`
+  display: none;
+`;
+
+const show = css`
+  display: flex;
+  margin-right: 30px !important;
+  width: 95%;
+  height: 150px;
+  background-color: white;
+`;
+
+class InteractiveNotes extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showedContent: false
+        };
+
+        this.showContent = this.showContent.bind(this);
+    }
+
+    showContent() {
+        this.setState({
+            showedContent: !this.state.showedContent
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Notes>
+                    {this.props.children}
+                    <button onClick={this.showContent}>
+                        {this.state.showedContent ? 'Ausblenden' : 'Anzeigen'}
+                    </button>
+                </Notes>
+                <div className={this.state.showedContent ? show : hide}>
+                    Hello World
+                </div>
+            </div>
+
+        );
+    }
+}
 
 export const VideoDetails = ({onSetSidebarOpen}) => {
-    return(
+    return (
         <div>
             <VideoDetailsContainer>
                 <Navigation onSetSidebarOpen={onSetSidebarOpen}/>
                 <LeftSideDetails>
                     <VideoPlayContainer>
-                        <FontAwesomeIcon className={playVideoButton} icon="play-circle" />
-                        <img style={{height: '450px'}} src={recentVideo1} />
+                        <FontAwesomeIcon className={playVideoButton} icon="play-circle"/>
+                        <img style={{height: '450px'}} src={recentVideo1}/>
                     </VideoPlayContainer>
                     <VideoTitle>beuthBOX Video Titel</VideoTitle>
                     <div className={videoCaption}>
@@ -148,11 +226,46 @@ export const VideoDetails = ({onSetSidebarOpen}) => {
                                 <img src="https://via.placeholder.com/200x150?text=next video"/>
                                 <a href="#">Kapitel 5. Thema MNO</a>
                             </div>
+
+                            <div className={nextVideo}>
+                                <img src="https://via.placeholder.com/200x150?text=next video"/>
+                                <a href="#">Kapitel 6. Thema PRS</a>
+                            </div>
+
+                            <div className={nextVideo}>
+                                <img src="https://via.placeholder.com/200x150?text=next video"/>
+                                <a href="#">Kapitel 7. Thema TUV</a>
+                            </div>
+
+                            <div className={nextVideo}>
+                                <img src="https://via.placeholder.com/200x150?text=next video"/>
+                                <a href="#">Kapitel 8. Thema WYZ</a>
+                            </div>
                         </div>
                     </div>
                 </LeftSideDetails>
                 <RightSideDetails>
-                    <div>bla</div>
+                    <Tags>
+                        <p style={{color: 'white'}}>#software</p>
+                        <p style={{color: '#00A5A5'}}>#teaching</p>
+                        <p style={{color: 'red'}}>#technology</p>
+                        <p style={{color: 'white'}}>#beuth</p>
+                        <p style={{color: '#00A5A5'}}>#beuthBox</p>
+                        <p style={{color: 'red'}}>#lecture</p>
+                    </Tags>
+                    <InteractiveNotes>
+                        <p>Notizen</p>
+                    </InteractiveNotes>
+                    <InteractiveNotes>
+                        <p>Feedback an Dozenten</p>
+                    </InteractiveNotes>
+                    <InteractiveNotes>
+                        <p>Kommentare</p>
+                    </InteractiveNotes>
+                    <InteractiveNotes>
+                        <p>Chat</p>
+                    </InteractiveNotes>
+
                 </RightSideDetails>
             </VideoDetailsContainer>
             <Footer>
