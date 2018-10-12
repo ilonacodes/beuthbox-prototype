ff058598903fd567ceab157374a3a46477f45508
Ilona Demidenko <demiluri@gmail.com>
2018-10-12 20:56:14 +0200

    Updates

---
+++ precache-manifest.4daaa4998f658230f46f81ccda513e1a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3dea132f3b1218fd2073b6ddd9cf795",
+    "url": "/static/media/recent-video-1.f3dea132.jpg"
+  },
+  {
+    "revision": "ff1546acf783472dfe37",
+    "url": "/static/css/main.18246563.chunk.css"
+  },
+  {
+    "revision": "d8c9c413789a1c57b4be",
+    "url": "/static/js/1.d8c9c413.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f8c41fe700513f3c89c24783bb9ea6a",
+    "url": "/static/media/facebook.9f8c41fe.png"
+  },
+  {
+    "revision": "80323c10dc2afa1f86d11b393470539c",
+    "url": "/static/media/instagram.80323c10.png"
+  },
+  {
+    "revision": "ff1546acf783472dfe37",
+    "url": "/static/js/main.ff1546ac.chunk.js"
+  },
+  {
+    "revision": "6beb53f6bee3f6ed5c7300519de1332b",
+    "url": "/static/media/recent-video-2.6beb53f6.jpeg"
+  },
+  {
+    "revision": "609e17fa23de029ef98fbc82a9c52226",
+    "url": "/static/media/recent-video-3.609e17fa.jpeg"
+  },
+  {
+    "revision": "caa5d622b23b73998f14b504dba6d1ee",
+    "url": "/static/media/arrow-right.svg.caa5d622.png"
+  },
+  {
+    "revision": "a9644974e0affd2282a59100bd5604bb",
+    "url": "/static/media/arrow-left.svg.a9644974.png"
+  },
+  {
+    "revision": "c41590a5773e08a5fd6e412a58c20b55",
+    "url": "/index.html"
+  }
+];
